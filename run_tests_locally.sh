be3aabb151e398fa4da7d400ca5706752cd3458d
Rinat Mustafaev <rinat.mustafaev@konux.de>
2024-05-06 17:35:40 +0200

    Move logic of checking that web is up to a test itself

--- run_tests_locally.sh
+++ run_tests_locally.sh
@@ -11,21 +11,6 @@ poetry run python3 manage.py migrate
 poetry run python3 manage.py runserver  &
 SERVER_PID=$!
 
-# Wait for server to start
-echo "Waiting for server to start"
-max_attempts=10
-for i in $(seq 1 $max_attempts); do
-  if curl --silent --fail http://localhost:8000/api/health > /dev/null; then
-    echo "Server started"
-    break
-  elif [ $i -eq $max_attempts ]; then
-    echo "Failed to connect to server after $max_attempts attempts, exiting"
-    exit 1
-  else
-    sleep 1
-  fi
-done
-
 poetry run python3 integration-tests/response-check.py
 kill $SERVER_PID
 docker-compose -f infra/docker-compose.yml down
